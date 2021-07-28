69ebb1ed31eb264935a3b832bde65f4c1d7d6df8
CatherineCao2016 <caoying0824@gmail.com>
2021-07-28 16:55:01 -0400

    Update r_job.R

--- assets/rstudio/r_job.R
+++ assets/rstudio/r_job.R
@@ -2,6 +2,9 @@ library("pmml")
 
 library(nnet)
 
+library("projectLib")
+project <- access_project()
+
 ird <- data.frame(rbind(iris3[,,1], iris3[,,2], iris3[,,3]),
                   species = factor(c(rep("s",50), rep("c", 50), rep("v", 50))))
 
@@ -11,8 +14,6 @@ load_model <- readRDS(gzcon(rawConnection(project$get_file("rmodel.rds"))))
 
 final_predictions <- predict(load_model, ird[1:5,])
 
-library("projectLib")
-project <- access_project()
 
 csv_lines <- capture.output(write.csv(final_predictions, row.names=FALSE), type="output")
 csv_raw <- charToRaw(paste0(csv_lines, collapse='\n'))
