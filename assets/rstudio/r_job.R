9e13ca2e190646d9fbc8f48246b5161dc0c096a4
CatherineCao2016 <caoying0824@gmail.com>
2021-07-28 16:15:30 -0400

    Update r_job.R

--- assets/rstudio/r_job.R
+++ assets/rstudio/r_job.R
@@ -5,11 +5,15 @@ library(nnet)
 ird <- data.frame(rbind(iris3[,,1], iris3[,,2], iris3[,,3]),
                   species = factor(c(rep("s",50), rep("c", 50), rep("v", 50))))
 
-list.files('/')
 
-
-#load_model <- readRDS("assets/rstudio/rmodel.rds")
+load_model <- readRDS("/project_data/data_asset/rmodel.rds")
 
 final_predictions <- predict(load_model, ird[1:5,])
 
-write.csv(final_predictions, file = "r_output.csv")
+library("projectLib")
+project <- access_project()
+
+csv_lines <- capture.output(write.csv(final_predictions, row.names=FALSE), type="output")
+csv_raw <- charToRaw(paste0(csv_lines, collapse='\n'))
+
+project$save_data("r_job_save_output.csv", csv_raw, overwrite=TRUE)
