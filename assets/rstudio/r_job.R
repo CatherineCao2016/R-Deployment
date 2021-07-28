9f7470fbe83e5b7a0da4d0b9d54f782d1c8cea99
CatherineCao2016 <caoying0824@gmail.com>
2021-07-28 15:45:22 -0400

    Update r_job.R

--- assets/rstudio/r_job.R
+++ assets/rstudio/r_job.R
@@ -5,7 +5,10 @@ library(nnet)
 ird <- data.frame(rbind(iris3[,,1], iris3[,,2], iris3[,,3]),
                   species = factor(c(rep("s",50), rep("c", 50), rep("v", 50))))
 
-load_model <- readRDS("assets/rstudio/rmodel.rds")
+list.files('/')
+
+
+#load_model <- readRDS("assets/rstudio/rmodel.rds")
 
 final_predictions <- predict(load_model, ird[1:5,])
 
