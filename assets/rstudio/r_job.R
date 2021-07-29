12e21f0fdd83122893686cbdc2e6bdb3bc5dc087
CatherineCao2016 <caoying0824@gmail.com>
2021-07-29 10:30:58 -0400

    Update r_job.R

--- assets/rstudio/r_job.R
+++ assets/rstudio/r_job.R
@@ -2,8 +2,13 @@ library("pmml")
 
 library(nnet)
 
-library("projectLib")
+library(projectLib)
+Sys.setenv(PROJECT_ID=Sys.getenv('DSX_PROJECT_ID'))
+Sys.setenv(PROJECT_ACCESS_TOKEN=Sys.getenv('DSX_TOKEN'))
+Sys.setenv(RUNTIME_ENV_APSX_URL='https://internal-nginx-svc:12443')
+httr::set_config(httr::config(cainfo = "/etc/certificate/certificate.pem"))
 project <- access_project()
+
 
 ird <- data.frame(rbind(iris3[,,1], iris3[,,2], iris3[,,3]),
                   species = factor(c(rep("s",50), rep("c", 50), rep("v", 50))))
@@ -12,10 +17,12 @@ ird <- data.frame(rbind(iris3[,,1], iris3[,,2], iris3[,,3]),
 #load_model <- readRDS("/project_data/data_asset/rmodel.rds")
 load_model <- readRDS(gzcon(rawConnection(project$get_file("rmodel.rds"))))
 
+
+
 final_predictions <- predict(load_model, ird[1:5,])
 
 
 csv_lines <- capture.output(write.csv(final_predictions, row.names=FALSE), type="output")
 csv_raw <- charToRaw(paste0(csv_lines, collapse='\n'))
 
-project$save_data("r_job_save_output.csv", csv_raw, overwrite=TRUE)
+project$save_data("r_job_save_output_july29.csv", csv_raw, overwrite=TRUE)
